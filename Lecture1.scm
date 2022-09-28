fa85d03095f096efe9bf2d49cc140d143d7c45d5
DohnJoe-ost <dskrt4@gmail.com>
2022-09-28 11:10:32 +0100

    Create Lecture1.scm

---
+++ Lecture1.scm
@@ -0,0 +1,16 @@
+(define aNumber 10)
+(define anotherNumber 20)
+(define answer (+ aNumber anotherNumber))
+              
+ 
+(define answer (+ aNumber anotherNumber))
+answer
+=> 30
+(define div)
+(div 2 3)
+#<undef> is not a function [div]
+(define div /)
+(div 2 3)
+=> 0.6666666666666666
+biwascheme> 
+ 
